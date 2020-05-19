dbebbb7be66df7df2eb83d0bc97802fcb387b71c
Salvatore Facista <sfacista@tgen.org>
2020-05-18 18:45:25 -0700

    added logic for mutation_type

--- find_a_variant_by_protein_index.sql
+++ find_a_variant_by_protein_index.sql
@@ -1,3 +1,3 @@
 SELECT * 
 FROM cbioportal_variant.variants 
-WHERE cbioportal_variant.variants.Chromosome = '1'  AND cbioportal_variant.variants.`Protein Change` LIKE '_1579_';
+WHERE cbioportal_variant.variants.Chromosome = '1'  AND cbioportal_variant.variants.`Protein Change` LIKE '_1579_' AND cbioportal_variant.variants.`Mutation Type` LIKE 'Nonsense_Mutation' ;
